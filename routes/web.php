3bc8f867cee597022e28504cada96fccefeac024
algarva <algarva488@gmail.com>
2024-09-21 23:26:05 -0700

    logica formulario sillas hecho

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SillasController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('formulariosillas'); 
 });
+
+//fomulario sillas
+Route::post('/formulariosillas', [SillasController::class, 'insertarsilla'])->name('silla.insertarsilla');
