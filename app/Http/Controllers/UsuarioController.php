1698c36e653943bf92b0ecb5af90d79f77128b1a
algarva <algarva488@gmail.com>
2024-09-22 19:46:01 -0700

    se agregaron los pos perdidos a web

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -7,13 +7,4 @@ use Illuminate\Http\Request;
 class UsuarioController extends Controller
 {
     // php artisan make:controller UsuarioController
- php artisan make:controller  UsuarioController
- php artisan make:controller  SillasController
- php artisan make:controller  MesasController
- php artisan make:controller  MantelesController
- php artisan make:controller  BrincolinesController
- php artisan make:controller  MotoresController
- php artisan make:controller  ExtencionesController
- php artisan make:controller  InventarioController
- php artisan make:controller  RentasController
 }
