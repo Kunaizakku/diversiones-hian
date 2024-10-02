f56bfc1577ae31568ff743ea045d7a54b84e912d
Edwin <ediazochoa70@gmail.com>
2024-10-02 09:39:01 -0700

    forms mesas y extenciones (diseño)

---
+++ resources/views/form_extenciones.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Registro de Extensiones</title>
+</head>
+<body>
+
+  @include('sidebar')
+  
+  <div class="form-container">
+      <p class="title">Registro de Extensiones</p>
+      <form class="form" id="form-register-extensiones" action="" method="post" enctype="multipart/form-data">
+          @csrf
+          
+          <!-- Campo para el nombre de la extensión -->
+          <div class="input-group">
+            <label for="nombre_extension">Nombre de la Extensión</label>
+            <input type="text" name="nombre_extension" id="nombre_extension" placeholder="Ingresa el nombre de la extensión" required>
+          </div>
+          
+          <!-- Campo para la imagen de la extensión -->
+          <div class="input-group">
+            <label for="imagen_extension">Imagen de la Extensión</label>
+            <input type="file" name="imagen_extension" id="imagen_extension" required>
+          </div>
+
+          <!-- Campo para la cantidad de extensiones -->
+          <div class="input-group">
+            <label for="cantidad_extension">Cantidad de Extensiones</label>
+            <input type="number" name="cantidad_extension" id="cantidad_extension" placeholder="Ingresa la cantidad de extensiones" min="1" required>
+          </div>
+          
+          <!-- Botón para enviar el formulario -->
+          <button class="sign">Registrar Extensión</button>
+      </form>
+  </div>
+
+  @include('fooder')
+
+</body>
+</html>
---
+++ resources/views/form_manteles.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Registro de Manteles</title>
+</head>
+<body>
+
+  @include('sidebar')
+  
+  <div class="form-container">
+      <p class="title">Registro de Manteles</p>
+      <form class="form" id="form-register-manteles" action="" method="post" enctype="multipart/form-data">
+          @csrf
+          
+          <!-- Campo para la imagen del mantel -->
+          <div class="input-group">
+            <label for="imagen_mantel">Imagen del Mantel</label>
+            <input type="file" name="imagen_mantel" id="imagen_mantel" required>
+          </div>
+          
+          <!-- Campo para el color del mantel -->
+          <div class="input-group">
+            <label for="color_mantel">Color del Mantel</label>
+            <input type="text" name="color_mantel" id="color_mantel" placeholder="Ingresa el color del mantel" required>
+          </div>
+
+          <!-- Campo para el tipo de mantel -->
+          <div class="input-group">
+            <label for="tipo_mantel">Tipo de Mantel</label>
+            <select name="tipo_mantel" id="tipo_mantel" required>
+              <option value="redondo">Redondo</option>
+              <option value="cuadrado">Cuadrado</option>
+              <option value="rectangular">Rectangular</option>
+            </select>
+          </div>
+
+          <!-- Campo para la cantidad de manteles -->
+          <div class="input-group">
+            <label for="cantidad_mantel">Cantidad de Manteles</label>
+            <input type="number" name="cantidad_mantel" id="cantidad_mantel" placeholder="Ingresa la cantidad de manteles" min="1" required>
+          </div>
+          
+          <!-- Botón para enviar el formulario -->
+          <button class="sign">Registrar Mantel</button>
+      </form>
+  </div>
+
+  @include('fooder')
+
+</body>
+</html>
