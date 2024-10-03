cb7a5c69b434114e89b8378eeb52b730afe50333
algarva <algarva488@gmail.com>
2024-10-02 22:27:06 -0700

    formulari oextencion ya agarra

--- app/Http/Controllers/ExtencionesController.php
+++ app/Http/Controllers/ExtencionesController.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
+use \App\Models\Extenciones;
 
 use Illuminate\Http\Request;
 
 class ExtencionesController extends Controller
 {
-    //
+    public function insertarextenciones (Request $request) {
+
+        $extenciones = new Extenciones;
+
+        if ($request->hasFile('imagen_extenciones')) {
+            $imagen = $request->file('imagen_extenciones');
+            $rutaimagen = $imagen->store('public/images');
+            $extenciones->imagen_extenciones = str_replace('public/', '', $rutaimagen);
+        }
+
+        $extenciones->nombre_extenciones = $request->nombre_extenciones;
+        $extenciones->cant_extenciones = $request->cant_extenciones;
+        $extenciones->estatus_extenciones = 1;
+
+        $extenciones->save();
+        return redirect('/form_extenciones');
+    }
 }
--- resources/views/form_extenciones.blade.php
+++ resources/views/form_extenciones.blade.php
@@ -12,25 +12,25 @@
   
   <div class="form-container">
       <p class="title">Registro de Extensiones</p>
-      <form class="form" id="form-register-extensiones" action="" method="post" enctype="multipart/form-data">
+      <form class="form" id="form-register-extensiones" action="{{ route('extencion.insertarextenciones') }}" method="post" enctype="multipart/form-data">
           @csrf
           
           <!-- Campo para el nombre de la extensión -->
           <div class="input-group">
             <label for="nombre_extension">Nombre de la Extensión</label>
-            <input type="text" name="nombre_extension" id="nombre_extension" placeholder="Ingresa el nombre de la extensión" required>
+            <input type="text" name="nombre_extenciones" id="nombre_extenciones" placeholder="Ingresa el nombre de la extensión" required>
           </div>
           
           <!-- Campo para la imagen de la extensión -->
           <div class="input-group">
             <label for="imagen_extension">Imagen de la Extensión</label>
-            <input type="file" name="imagen_extension" id="imagen_extension" required>
+            <input type="file" name="imagen_extenciones" id="imagen_extenciones" required>
           </div>
 
           <!-- Campo para la cantidad de extensiones -->
           <div class="input-group">
             <label for="cantidad_extension">Cantidad de Extensiones</label>
-            <input type="number" name="cantidad_extension" id="cantidad_extension" placeholder="Ingresa la cantidad de extensiones" min="1" required>
+            <input type="number" name="cant_extenciones" id="cant_extenciones" placeholder="Ingresa la cantidad de extensiones" min="1" required>
           </div>
           
           <!-- Botón para enviar el formulario -->
