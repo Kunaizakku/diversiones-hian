9c0fa09a48d6a494683c9dfd8226de5fc120f298
algarva <algarva488@gmail.com>
2024-11-21 20:51:09 -0700

    pwa instalable funcionando, no le muevan porfavor xd

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,6 +9,11 @@
   <link rel="stylesheet" href="{{ asset('css/tablas.css') }}?v=1">
   <link rel="stylesheet" href="{{ asset('css/formularios.css') }}?v=1">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- PWA  -->
+    <meta name="theme-color" content="#6777ef"/>
+    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
 </head>
 <body>
 
@@ -110,4 +115,23 @@
 @include('fooder')
 
 </body>
+
+
+<script src="{{ asset('/sw.js') }}"></script>
+<script>
+   if ("serviceWorker" in navigator) {
+      // Register a service worker hosted at the root of the
+      // site using the default scope.
+      navigator.serviceWorker.register("/sw.js").then(
+      (registration) => {
+         console.log("Service worker registration succeeded:", registration);
+      },
+      (error) => {
+         console.error(`Service worker registration failed: ${error}`);
+      },
+    );
+  } else {
+     console.error("Service workers are not supported.");
+  }
+</script>
 </html>
